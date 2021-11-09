57d430c7209223893a80e00843f3561235603b4c
Amine HOUSSANE <aminehoussane@gmail.com>
2021-11-09 13:31:36 +0100

    Admin Profile & image update part 2

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -25,20 +25,15 @@
                                             <div class="form-group">
                                                 <h5>Admin User Name <span class="text-danger">*</span></h5>
                                                 <div class="controls">
-                                                    <input type="text" name="name" class="form-control" required="">
+                                                    <input type="text" name="name" class="form-control" required="" value="{{ $editData->name}}">
                                                 </div>
                                             </div>
                                         </div><!-- end col md 6  -->
-                                    </div><!-- end row -->
-
-
-
-                                    <div class="row">
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <h5>Admin Email <span class="text-danger">*</span></h5>
                                                 <div class="controls">
-                                                    <input type="email" name="email" class="form-control" required="">
+                                                    <input type="email" name="email" class="form-control" required="" value="{{ $editData->email}}">
                                                 </div>
                                             </div>
                                         </div><!-- end col md 6  -->
@@ -46,17 +41,20 @@
 
                                     <div class="row">
                                         <div class="col-md-6">
-
+                                            <div class="form-group">
+                                                <h5>Profile Image <span class="text-danger">*</span></h5>
+                                                <div class="controls">
+                                                    <input type="file" name="profile_photo_path" class="form-control" required="">
+                                                    <div class="help-block"></div>
+                                                </div>
+                                            </div>
                                         </div><!-- end col md 6  -->
+                                        <div class="col-md-6">
+
+                                            <img src="{{ (!empty($editData->profile_photo_path))? url('upload/admin_images/'.$editData->profile_photo_path): url('upload/no_image.jpg/') }}" style="width: 100px;height: 100px">
+                                        </div>
                                     </div><!-- end row -->
 
-                                    <div class="form-group">
-                                        <h5>File Input Field <span class="text-danger">*</span></h5>
-                                        <div class="controls">
-                                            <input type="file" name="file" class="form-control" required="">
-                                            <div class="help-block"></div>
-                                        </div>
-                                    </div>
                                     <div class="text-xs-right">
                                         <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Update"></input>
                                     </div>
