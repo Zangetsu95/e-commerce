c057494bb4032549342ee55dad7a9c09aa6d57b4
Amine HOUSSANE <aminehoussane@gmail.com>
2021-11-22 15:11:27 +0100

    Admin profile change password part 2

--- app/Http/Controllers/Backend/AdminProfileController.php
+++ app/Http/Controllers/Backend/AdminProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Auth;
+
 
 
 class AdminProfileController extends Controller
@@ -56,7 +59,25 @@ class AdminProfileController extends Controller
     public function AdminUpdateChangePassword(Request $request)
     {
 
+        $validateDAta = $request->validate([
 
+            'oldpassword' => 'required',
+            'password' => 'required|confirmed',
+        ]);
 
+        $hashedPassword = Admin::find(1)->password;
+        if (Hash::check($request->oldpassword, $hashedPassword)) {
+
+            $admin = Admin::find(1);
+            $admin->password = Hash::make($request->password);
+            $admin->save();
+            Auth::logout();
+            return redirect()->route('admin.logout');
+            
+        } else {
+
+            return redirect()->back();
+            
+        }
     }
 }
