9f20ac92a88666623c156aac9e2a4b17e8b5bc2f
Amine HOUSSANE <aminehoussane@gmail.com>
2021-12-01 16:04:50 +0100

    Add Active Sidebar Menu

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -1,3 +1,10 @@
+@php
+    $prefix = Request::route()->getPrefix();
+    $route = Route::current()->getName();
+
+@endphp
+
+
 <aside class="main-sidebar">
     <!-- sidebar-->
     <section class="sidebar">
@@ -17,14 +24,14 @@
         <!-- sidebar menu-->
         <ul class="sidebar-menu" data-widget="tree">
 
-            <li>
-                <a href="index.html">
+            <li class="{{ ($route == 'dashboard') ? 'active' : '' }}">
+                <a href="{{ url('admin/dashboard')}}">
                     <i data-feather="pie-chart"></i>
                     <span>Dashboard</span>
                 </a>
             </li>
 
-            <li class="treeview">
+            <li class="treeview {{ ($prefix == '/brand') ? 'active' : '' }}">
                 <a href="#">
                     <i data-feather="message-circle"></i>
                     <span>Brand</span>
@@ -33,8 +40,7 @@
                     </span>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a href="{{ route('all.brand')}}"><i class="ti-more"></i>All Brand</a></li>
-                    <li><a href="calendar.html"><i class="ti-more"></i>Calendar</a></li>
+                    <li class="{{ ($route == 'all.brand') ? 'active' : '' }}"><a href="{{ route('all.brand')}}"><i class="ti-more"></i>All Brand</a></li>
                 </ul>
             </li>
 
