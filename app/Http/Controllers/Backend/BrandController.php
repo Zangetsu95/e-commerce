dd869b1809f743ab6434d2d493c4270bd8affc8a
Amine HOUSSANE <aminehoussane@gmail.com>
2021-12-02 13:07:05 +0100

    Brand Crud Part 1

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -5,17 +5,48 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Brand;
+use Image;
 
 class BrandController extends Controller
 {
     public function BrandView()
     {
         $brands = Brand::latest()->get();
-        return view('backend.brand.brand_view',compact('brands'));
+        return view('backend.brand.brand_view', compact('brands'));
     }
 
-    public function BrandStore()
+    public function BrandStore(Request $request)
     {
-        
+        $request->validate([
+            'brand_name_en' => 'required',
+            'brand_name_fr' => 'required',
+            'brand_image' => 'required',
+        ], [
+            'brand_name_en.required' => 'Input Brand English Name',
+            'brand_name_fr.required' => 'Input Brand French Name',
+
+        ]);
+
+        //création d'un id + nom unique avec l'extension de l'image
+        $image = $request->file('brand_image');
+        $name_eng = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+        //l'extension laravel que nous avons installé
+        Image::make($image)->resize(300, 300)->save('upload/brand/' . $name_eng);
+        $save_url = 'upload/brand/' . $name_eng;
+
+        Brand::insert([
+            'brand_name_en' => $request->brand_name_en,
+            'brand_name_fr' => $request->brand_name_fr,
+            'brand_slug_eng' => strtolower(str_replace('.', '-', $request->brand_name_en)),
+            'brand_slug_fr' => str_replace('.', '-', $request->brand_name_fr),
+            'brand_image' =>   $save_url,
+        ]);
+
+        $notifications = array(
+            'message' => 'Brand inserted SuccessFuly !',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notifications);
     }
 }
--- resources/views/backend/brand/brand_view.blade.php
+++ resources/views/backend/brand/brand_view.blade.php
@@ -87,6 +87,9 @@
                                         <h5>Brand Name English <span class="text-danger">*</span></h5>
                                         <div class="controls">
                                             <input type="text" name="brand_name_en" class="form-control">
+                                            @error('brand_name_en')
+                                            <span class="text-danger">{{$message}} </span>
+                                            @enderror
                                         </div>
                                     </div>
 
@@ -94,6 +97,9 @@
                                         <h5>Brand Name French <span class="text-danger">*</span></h5>
                                         <div class="controls">
                                             <input type="text" name="brand_name_fr" class="form-control">
+                                            @error('brand_name_fr')
+                                            <span class="text-danger">{{$message}} </span>
+                                            @enderror
                                         </div>
                                     </div>
 
@@ -101,12 +107,15 @@
                                         <h5>Brand Images <span class="text-danger">*</span></h5>
                                         <div class="controls">
                                             <input type="file" name="brand_image" class="form-control">
+                                            @error('brand_image')
+                                            <span class="text-danger">{{$message}} </span>
+                                            @enderror
                                         </div>
                                     </div>
 
                                     <div class="text-xs-right">
                                         <input type="submit" class="btn btn-rounded btn-primary mb-5"
-                                            value="Update"></input>
+                                            value="Add New "></input>
                                     </div>
 
                             </div>
