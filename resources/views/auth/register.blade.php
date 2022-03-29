9f5125d4fdba2c76632641afe42fe340928f2206
Amine HOUSSANE <aminehoussane@gmail.com>
2022-03-29 13:25:15 +0200

    changing template frontend

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,121 +1,88 @@
 @extends('frontend.main_master')
 @section('content')
 
-<div class="breadcrumb">
-    <div class="container">
-        <div class="breadcrumb-inner">
-            <ul class="list-inline list-unstyled">
-                <li><a href="home.html">Home</a></li>
-                <li class='active'>Login</li>
-            </ul>
-        </div><!-- /.breadcrumb-inner -->
-    </div><!-- /.container -->
-</div><!-- /.breadcrumb -->
-
-<div class="body-content">
-    <div class="container">
-        <div class="sign-in-page">
+<main class="main pages">
+    <div class="page-header breadcrumb-wrap">
+        <div class="container">
+            <div class="breadcrumb">
+                <a href="index.html" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
+                <span></span> Pages <span></span> My Account
+            </div>
+        </div>
+    </div>
+    <div class="page-content pt-150 pb-150">
+        <div class="container">
             <div class="row">
-                <!-- Sign-in -->
-                <div class="col-md-6 col-sm-6 sign-in">
-                    <h4 class="">Sign in</h4>
-                    <p class="">Hello, Welcome to your account.</p>
-                    <div class="social-sign-in outer-top-xs">
-                        <a href="#" class="facebook-sign-in"><i class="fa fa-facebook"></i> Sign In with Facebook</a>
-                        <a href="#" class="twitter-sign-in"><i class="fa fa-twitter"></i> Sign In with Twitter</a>
+                <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-8">
+                            <div class="login_wrap widget-taber-content background-white">
+                                <div class="padding_eight_all bg-white">
+                                    <div class="heading_s1">
+                                        <h1 class="mb-5">Create an Account</h1>
+                                        <p class="mb-30">Already have an account? <a href="{{ route('login')}}">Login</a></p>
+                                    </div>
+                                    <form method="POST" action="{{ route('register') }}">
+                                        @csrf
+                                        <div class="form-group">
+                                            <input type="text" id="name" name="name" placeholder="Name" />
+                                            @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message}}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+
+                                        <div class="form-group">
+                                            <input type="email" id="email" name="email" placeholder="Email" />
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message}}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+
+                                        <div class="form-group">
+                                            <input type="text" id="phone" name="phone" placeholder="Phone Number" />
+                                            @error('phone')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message}}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+
+
+                                        <div class="form-group">
+                                            <input type="password" id="password" name="password" placeholder="Password" />
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message}}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+
+                                        <div class="form-group">
+                                            <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirm password" />
+                                            @error('password_confirmation')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message}}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group mb-30">
+                                            <button type="submit" class="btn btn-fill-out btn-block hover-up font-weight-bold" name="login">Submit &amp; Register</button>
+                                        </div>
+                                        <p class="font-xs text-muted"><strong>Note:</strong>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our privacy policy</p>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <!-- FORM-->
-                    <form method="POST" action="{{ isset($guard) ? url($guard.'/login') : route('login') }}">
-                        @csrf
-
-                        <div class="form-group">
-                            <label class="info-title" for="exampleInputEmail1">Email Address <span>*</span></label>
-                            <input type="email" id="email" name="email" class="form-control unicase-form-control text-input">
-                        </div>
-                        <div class="form-group">
-                            <label class="info-title" for="exampleInputPassword1">Password <span>*</span></label>
-                            <input type="password" id="password" name="password" class="form-control unicase-form-control text-input">
-                        </div>
-                        <div class="radio outer-xs">
-                            <label>
-                                <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Remember me!
-                            </label>
-                            <a href="{{ route('password.request') }}" class="forgot-password pull-right">Forgot your Password?</a>
-                        </div>
-                        <button type="submit" class="btn-upper btn btn-primary checkout-page-button">Login</button>
-                    </form>
-                    <!-- END FORM-->
                 </div>
-                <!-- Sign-in -->
-
-                <!-- create a new account -->
-                <div class="col-md-6 col-sm-6 create-new-account">
-                    <h4 class="checkout-subtitle">Create a new account</h4>
-                    <p class="text title-tag-line">Create your new account.</p>
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group">
-                            <label class="info-title" for="exampleInputEmail1">Name <span>*</span></label>
-                            <input type="text" id="name" name="name" class="form-control unicase-form-control text-input">
-                            @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message}}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label class="info-title" for="exampleInputEmail2">Email Address <span>*</span></label>
-                            <input type="email" id="email" name="email" class="form-control unicase-form-control text-input">
-                            @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message}}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label class="info-title" for="exampleInputEmail1">Phone Number <span>*</span></label>
-                            <input type="text" id="phone" name="phone" class="form-control unicase-form-control text-input">
-                            @error('phone')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message}}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label class="info-title" for="exampleInputEmail1">Password <span>*</span></label>
-                            <input type="password" id="password" name="password" class="form-control unicase-form-control text-input">
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message}}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label class="info-title" for="exampleInputEmail1">Confirm Password <span>*</span></label>
-                            <input type="password" id="password_confirmation" name="password_confirmation" class="form-control unicase-form-control text-input">
-                            @error('password_confirmation')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message}}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <button type=" submit" class="btn-upper btn btn-primary checkout-page-button">Sign Up</button>
-                    </form>
-
-
-                </div>
-                <!-- create a new account -->
-            </div><!-- /.row -->
-        </div><!-- /.sigin-in-->
-        <!-- ============================================== BRANDS CAROUSEL ============================================== -->
-
-
-        @include('frontend.body.brand')
-
-        <!-- ============================================== BRANDS CAROUSEL : END ============================================== -->
-    </div><!-- /.container -->
-</div><!-- /.body-content -->
+            </div>
+        </div>
+    </div>
+</main>
 
 
 
