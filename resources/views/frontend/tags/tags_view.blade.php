dd43d3abea6552d0e1d38a254015c8e43b633f1f
Amine HOUSSANE <aminehoussane@gmail.com>
2022-04-15 15:18:43 +0200

    showing product with tags

--- resources/views/frontend/tags/tags_view.blade.php
+++ resources/views/frontend/tags/tags_view.blade.php
@@ -31,7 +31,7 @@
                                     <ul>
                                         @foreach ($categories as $category)
                                             <li>
-                                                <a href="shop-grid-right.html"> <img
+                                                <a href="#collapse{{ $category->id }}"> <img
                                                         src="assets/imgs/theme/icons/category-1.svg" alt="" />
                                                     @if (session()->get('language') == 'french')
                                                         {{ $category->category_name_fr }}
@@ -180,13 +180,14 @@
                     </div>
                 </div>
                 <div class="row product-grid">
-                    <div class="col-lg-1-5 col-md-4 col-12 col-sm-6">
+                    @foreach ( $products as $product )
+                    <div class="col-lg-1-5 col-md-4 col-12 col-sm-6" id="collapse">
                         <div class="product-cart-wrap mb-30">
                             <div class="product-img-action-wrap">
                                 <div class="product-img product-img-zoom">
-                                    <a href="shop-product-right.html">
+                                    <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en) }}">
                                         <img class="default-img"
-                                            src="{{ asset('frontend/assets/imgs/shop/product-1-1.jpg') }}" alt="" />
+                                            src="{{ asset($product->product_thambnail) }}" alt="" />
                                         <img class="hover-img" src="assets/imgs/shop/product-1-2.jpg" alt="" />
                                     </a>
                                 </div>
@@ -206,7 +207,13 @@
                                 <div class="product-category">
                                     <a href="shop-grid-right.html">Snack</a>
                                 </div>
-                                <h2><a href="shop-product-right.html">Seeds of Change Organic Quinoe Naturel</a></h2>
+                                <h2><a href="shop-product-right.html">
+                                    @if (session()->get('language') == 'french')
+                                        {{ $product->product_name_fr }}
+                                    @else
+                                        {{ $product->product_name_en }}
+                                    @endif
+                                </a></h2>
                                 <div class="product-rate-cover">
                                     <div class="product-rate d-inline-block">
                                         <div class="product-rating" style="width: 90%"></div>
@@ -214,22 +221,38 @@
                                     <span class="font-small ml-5 text-muted"> (4.0)</span>
                                 </div>
                                 <div>
-                                    <span class="font-small text-muted">By <a
-                                            href="vendor-details-1.html">NestFood</a></span>
+                                    {{-- <span class="font-small text-muted">By <a
+                                            href="vendor-details-1.html">NestFood</a></span> --}}
                                 </div>
                                 <div class="product-card-bottom">
+                                    @php
+                                    $amount = $product->selling_price - $product->discount_price;
+                                    $discount = ($amount/$product->selling_price) * 100;
+                                        @endphp
+                                    @if($product->discount_price == NULL)
                                     <div class="product-price">
-                                        <span>$28.85</span>
-                                        <span class="old-price">$32.8</span>
+                                        <span>{{ $product->selling_price }}€</span>
                                     </div>
                                     <div class="add-cart">
                                         <a class="add" href="shop-cart.html"><i
                                                 class="fi-rs-shopping-cart mr-5"></i>Add </a>
                                     </div>
+                                    @else
+                                    <div class="product-price">
+                                        <span>{{ $amount }}€</span>
+                                        <span class="old-price">{{ $product->selling_price }}€</span>
+                                    </div>
+
+                                    <div class="add-cart">
+                                        <a class="add" href="shop-cart.html"><i
+                                            class="fi-rs-shopping-cart mr-5"></i>Add </a>
+                                    </div>
+                                    @endif
                                 </div>
                             </div>
                         </div>
                     </div>
+                    @endforeach
                     <!--end product card-->
 
                 </div>
