c14a5486c1dd68beb5830e398c8bf19c1bfb887b
Amine HOUSSANE <aminehoussane@gmail.com>
2022-05-09 13:22:16 +0200

    showing data of user for his order

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -9,5 +9,5 @@
                     </li>
                 </ul>
             </div>
-            &copy; 2020 <a href="#">Easy Shop</a>. All Rights Reserved.
+            &copy; 2022 <a href="#">ShinSekaiManga</a>. All Rights Reserved.
         </footer>
--- resources/views/frontend/user/order/order_view.blade.php
+++ resources/views/frontend/user/order/order_view.blade.php
@@ -1,66 +1,62 @@
 @extends('frontend.main_master')
 @section('content')
-<div class="body-content">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-2"><br><br>
-                <ul class="list-group list-group-flush">
+    <div class="body-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2"><br><br>
+                    <ul class="list-group list-group-flush">
 
-                    <a href="{{ route('dashboard') }}" class="btn btn-primary btn-sm btn-block">Dashboard</a>
-                </ul>
-            </div><!-- end col md 2 -->
+                        <a href="{{ route('dashboard') }}" class="btn btn-primary btn-sm btn-block">Dashboard</a>
+                    </ul>
+                </div><!-- end col md 2 -->
 
-            <div class="col-md-2">
+                <div class="col-md-2">
 
-            </div><!-- end col md 2 -->
+                </div><!-- end col md 2 -->
 
-            <div class="tab-pane " id="orders" role="tabpanel" aria-labelledby="orders-tab">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="mb-0">Your Orders</h3>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th>Order</th>
-                                        <th>Date</th>
-                                        <th>Status</th>
-                                        <th>Total</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>#1357</td>
-                                        <td>March 45, 2020</td>
-                                        <td>Processing</td>
-                                        <td>$125.00 for 2 item</td>
-                                        <td><a href="#" class="btn-small d-block">View</a></td>
-                                    </tr>
-                                    <tr>
-                                        <td>#2468</td>
-                                        <td>June 29, 2020</td>
-                                        <td>Completed</td>
-                                        <td>$364.00 for 5 item</td>
-                                        <td><a href="#" class="btn-small d-block">View</a></td>
-                                    </tr>
-                                    <tr>
-                                        <td>#2366</td>
-                                        <td>August 02, 2020</td>
-                                        <td>Completed</td>
-                                        <td>$280.00 for 3 item</td>
-                                        <td><a href="#" class="btn-small d-block">View</a></td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                <div class="tab-pane " id="orders" role="tabpanel" aria-labelledby="orders-tab">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="mb-0">Your Orders</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Order</th>
+                                            <th>Date</th>
+                                            <th>Status</th>
+                                            <th>Total</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($orders as $order)
+                                            <tr>
+                                                <td>{{ $order->invoice_no }}</td>
+                                                <td>{{ $order->order_date }}</td>
+                                                <td>
+                                                    <label for="">
+                                                        <span class="badge badge-pill badge-warning"
+                                                            style="background: #418DB9;">{{ $order->status }} </span>
+                                                    </label>
+                                                </td>
+                                                <td>{{ $order->amount }}€</td>
+                                                <td>
+                                                    <a href="#" class="btn-small d-block">View</a>
+                                                    <a href="#" class="btn-small d-block" style="color: red">Invoice</a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-        </div><!-- row -->
+            </div><!-- row -->
+        </div>
     </div>
-</div>
 @endsection
